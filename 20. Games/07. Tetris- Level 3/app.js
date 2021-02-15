b17c123d00ee99a40cb84dc729f096edf7869629
QuintaBCN <75229325+quintabcn@users.noreply.github.com>
2021-02-15 12:59:06 -0300

    tetris

---
+++ 20. Games/07. Tetris- Level 3/app.js
@@ -0,0 +1,127 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const grid= document.querySelector('.grid');
+let squares= Array.from(grid.querySelectorAll('div'));
+const width= 10;
+const height= 20;
+let currentPosition=4;
+
+// asign function to keycodes
+function control(e){
+    if(e.KeyCode === 39){
+        moveRight();
+    }else if (e.KeyCode === 38){
+        rotate()
+    }else if (e.KeyCode === 37){
+        moveLeft();
+    }else if (e.KeyCode === 40){
+        moveDown();
+    }
+}
+document.addEventListener('keyup', control);
+
+
+// The Tetronimoes
+const lTetromino= [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+]
+
+
+const zTetromino= [
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1],
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1]
+]
+
+const tTetromino= [
+    [1, width, width+1, width+2],
+    [1, width+1, width+2, width*2+1],
+    [width, width+1, width+2, width*2+1],
+    [1, width, width+1, width*2+1]
+]
+
+const oTetromino= [
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1]
+]
+
+const iTetromino= [
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3]
+]
+
+const theTetrominoes= [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+
+// Randomly select Tetromino
+let random= Math.floor(match.random()*theTetrominoes.length);
+let currentRotation=0;
+let current= theTetrominoes[random][currentRotation];
+
+
+// draw the shape
+function draw() {
+    current.forEach( index=> (
+        squares[currentPosition + index].classList.add('block')
+    ));
+}
+
+// undraw the shape
+function undraw() {
+    current.forEach( index => (
+        squares[currentPosition + index].classList.remove('block')
+    ));
+}
+
+// move down shape
+function moveDown(){
+    undraw();
+    currentPosition= currentPosition += width;
+    draw();
+    freeze();
+}
+
+// move left and prevent collisions width shapes moving left
+function moveRight(){
+    undraw();
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width- 1);
+    if(!isAtRightEdge) currentPosition += 1;
+    if (current.some(index => squares[currentPosition + index].classList.contains('block2'))){
+        currentPosition -=1;
+    }
+    draw();
+}
+
+function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
+    if(!isAtLeftEdge) currentPosition -=1;
+    if(current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+        currentPosition +=1;
+    }
+    draw();
+}
+
+// rotate Tetromino
+function rotate(){
+    undraw();
+    currentRotation ++;
+    if (currentRotation === current.length) {
+        currentRotation =0;
+    }
+    current=theTetrominoes[random][currentRotation];
+    draw();
+}
+
+draw();
+
+
+})
